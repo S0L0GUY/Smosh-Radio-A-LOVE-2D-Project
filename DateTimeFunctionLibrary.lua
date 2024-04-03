a5aa62b8c85f985904e577f891cc839ab42ed02f
Evan Grinnell <155865196+S0L0GUY@users.noreply.github.com>
2024-04-03 15:54:12 -0700

    Add files via upload
    spell corrected some files

---
+++ DateTimeFunctionLibrary.lua
@@ -0,0 +1,59 @@
+local dateTime = {}
+
+function dateTime.Start()
+    function dateTime.update_current_date() -- Updates the current time to the os's time
+        return os.date("*t")
+    end
+    
+    function dateTime.whole_date() -- prints the date as 11/11/24 format
+        local currentDate = dateTime.update_current_date()
+        return currentDate.month .. "/" .. currentDate.day .. "/" .. currentDate.year
+    end
+    
+    function dateTime.month() -- prints the current month
+        local currentDate = dateTime.update_current_date()
+        return currentDate.month
+    end
+    
+    function dateTime.day() -- prints the current day
+        local currentDate = dateTime.update_current_date()
+        return currentDate.day
+    end
+    
+    function dateTime.year() -- prints the current year
+        local currentDate = dateTime.update_current_date()
+        return currentDate.year
+    end
+    
+    function dateTime.minute() -- prints the current minute
+        local currentDate = dateTime.update_current_date()
+        if currentDate.min < 10 then
+            return "0" .. tostring(currentDate.min)
+        else
+            return tostring(currentDate.min)
+        end
+    end
+    
+    function dateTime.hour() -- prints the current hour
+        local currentDate = dateTime.update_current_date()
+        if tonumber(currentDate.hour) > 12 then -- changes the time from a 24 hr clock to a 12 hr clock
+            return tonumber(currentDate.hour) - 12
+        else
+            return tonumber(currentDate.hour)
+        end
+    end
+    
+    function dateTime.full_time() -- prints the hour and minute '3:45'
+        return tostring(dateTime.hour()) .. ":" .. tostring(dateTime.minute())
+    end
+    
+    function dateTime.am_pm() -- prints am/pm
+        if tonumber(dateTime.hour()) > 12 then -- checks if the hour is more than 12
+            return "pm"
+        else
+            return "am"
+        end
+    end
+end
+
+return dateTime
---
+++ DebugLibrary.lua
@@ -0,0 +1,30 @@
+local M = {}
+
+function M.log(message)
+    -- writes to debug_log.txt
+    local file = io.open("debug_log.txt", "a") -- Open log file in append mode
+    file:write(message .. "\n") -- Write message to log file
+    file:write("" .. "\n") -- Makes a blank space at the bottom of the line
+    file:write("--------------------------------" .. "\n") -- Creates the bottom divider
+    file:close() -- Closes log file
+
+    -- Writes to the termanal
+    print(message)
+    print("--------------------------------")
+end
+
+function M.clear()
+    local file = io.open("debug_log.txt", "w") -- Open file in write mode
+    if file then
+        file:write("") -- Writes an empty string to effectively clear the file
+        file:close()
+    else
+        M.log("Error: Could not open debug_log.txt for clearing") -- Logs the error to debug_log.txt
+        -----------------------
+        print("Error: Could not open debug_log.txt for clearing") -- Logs the error to the terminal
+        print("")
+        print("--------------------------------")
+    end
+end
+
+return M
